b7bb1948e295208cdf161f588e4d31246d8fe9cd
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-06-22 17:40:06 +0330

    Fixed Compare Version

--- BatFiles/AnyDesk_Install.bat
+++ BatFiles/AnyDesk_Install.bat
@@ -1,37 +1,52 @@
 @echo off
 setlocal
 
+:: Compare Version File
+
 :: Path to AnyDesk executable and installer
-set ANYDESK_PATH="C:\Program Files (x86)\AnyDesk"
-set ANYDESK_EXEC="C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
-set ANYDESK_INST="C:\Program Files (x86)\AnyDesk\AnyDesk(Install).exe"
+echo Setting Paths
+set AnyDesk_Path="C:\Program Files (x86)\AnyDesk"
+set AnyDesk_Exe="C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
+set AnyDesk_On_Server="\\%DEPLOYMENT_SERVER%\%DEPLOYMENT_PATH%\Software\AnyDesk\%ANYDESK_VERSION%\AnyDesk.exe"
 
 :: Function to get the installed version of AnyDesk
-for /f "delims=" %%i in ('%ANYDESK_EXEC% --version') do set INSTALLED_VERSION=%%i
-    goto required
+echo Getting The Installed Version
+for /f "delims=" %%i in ('%AnyDesk_Exe% --version') do set Installed_Version=%%i
+    goto compare
 
-:required
-for /f "delims=" %%v in ('%ANYDESK_INST% --version') do set REQUIRED_VERSION=%%v
+:compare
+echo Comparing Versions
+echo AnyDesk Required  : %ANYDESK_VERSION%
+echo AnyDesk Installed : %Installed_Version%
+for /f "delims=" %%i in ('#CompareVersion %ANYDESK_VERSION% %Installed_Version%') do set Install_Condition=%%i
     goto continue
 
 :continue
-if exist %ANYDESK_EXEC% (
-    if "%INSTALLED_VERSION%"=="%REQUIRED_VERSION%" (
-        echo AnyDesk is already installed with the required version.
-    ) else (
-        echo AnyDesk version does not match the required version.
-        echo Please update AnyDesk to version %REQUIRED_VERSION%.
-    )
+echo Instal Condition  : %Install_Condition%
+if %Install_Condition%==True (
+    echo AnyDesk is already installed with the required version or above.
 ) else (
-    echo AnyDesk is not installed. Installing AnyDesk...
-    %ANYDESK_INST% --install %ANYDESK_PATH% --start-with-win --create-desktop-icon
+    echo No AnyDesk Detected or AnyDesk is out of date.
+    echo Installing AnyDesk...
+    echo ----------------------
+    echo Closing Potential AnyDesk
+    taskkill /IM anydesk.exe /F
+    echo ----------------------
+    set "%AnyDesk_Install%=C:\Program Files (x86)\AnyDesk\AnyDesk-%ANYDESK_VERSION%.exe"
+    echo ----------------------
+    copy %AnyDesk_On_Server% %AnyDesk_Install% /Y
+    echo ----------------------
+    %AnyDesk_Install% --install %AnyDesk_Path% --start-with-win --create-desktop-icon
+    echo ----------------------
+    del %AnyDesk_Install% /q
+    echo ----------------------
+    %AnyDesk_Exe%
+    echo ----------------------
 )
 
-echo AnyDesk Installed : %INSTALLED_VERSION%
-echo AnyDesk Installer : %REQUIRED_VERSION%
-echo AnyDesk Path   : %ANYDESK_PATH%
-echo Insstaled Path : %ANYDESK_EXEC%
-echo Installer Path : %ANYDESK_INST%
-
+echo AnyDesk Folder    : %AnyDesk_Path%
+echo AnyDesk Exe       : %AnyDesk_Exe%
+echo AnyDesk Installer : %AnyDesk_Install%
+echo Server Path       : %AnyDesk_On_Server%
 
 endlocal
