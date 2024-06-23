b000234134fcea4addf17b5aec358e6670e93447
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-06-23 10:38:22 +0330

    Changing RealVNC Installer (Currently Not Working)

--- BatFiles/RealVNCServer_Install.bat
+++ BatFiles/RealVNCServer_Install.bat
@@ -1,13 +1,17 @@
 @echo off
 
-set versionFile="C:\Program Files\RealVNC\VNC Server\version.txt"
-set requiredVersion=7.10
+echo Setting Paths
+set RealVNC_Path="C:\Program Files\RealVNC\VNC Server"
+set RealVNC_Exe="C:\Program Files\RealVNC\VNC Server\vncserver.exe"
+set RealVNC_On_Server="\\%DEPLOYMENT_SERVER%\%DEPLOYMENT_PATH%\Software\RealVNC\%ANYDESK_VERSION%\Server\RealVNC.msi"
+set RealVNC_Install="C:\Program Files (x86)\AnyDesk\AnyDesk(Install).exe"
 
-if exist %versionFile% (    
-    set /p currentVersion=<%versionFile%
-) else (
-    set currentVersion=
-)
+:: Function to get the installed version of AnyDesk
+echo Getting The Installed Version
+for /f "delims=" %%i in ('%RealVNC_Exe% --version') do set Installed_Version=%%i
+    if "%Installed_Version%"=="" (set Installed_Version=0.0.0)
+    goto compare
+
 
 echo Current Version  : %currentVersion%
 echo Required Version : %requiredVersion%
@@ -15,7 +19,7 @@ echo Required Version : %requiredVersion%
 if "%currentVersion%" neq "%requiredVersion%" (
     echo Version is not up to date. Starting file copying...
     net stop "vncserver"
-    copy "%DEPLOYMENT_SERVER%\%DEPLOYMENT_PATH%\RealVNC\Server\Install\*" "C:\Program Files\RealVNC\VNC Server\" /Y
+    copy "%DEPLOYMENT_SERVER%\%DEPLOYMENT_PATH%\RealVNC\%REALVNC_VERSION%\Server\Files\*" "C:\Program Files\RealVNC\VNC Server\" /Y
     "C:\Program Files\RealVNC\VNC Server\vnclicense.exe" -add "C:\Program Files\RealVNC\VNC Server\license.txt"
     net start "vncserver"
 ) else (
