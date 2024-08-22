b520261cf662a928ea403208485f7e30ebc17211
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-08-22 12:05:36 +0330

    Added Password Methods

---
+++ PowerShellFiles/CreatePassword.ps1
@@ -0,0 +1,29 @@
+# Prompt the user to enter a password
+$password = Read-Host -Prompt "Enter the password" -AsSecureString
+
+# Convert the SecureString password to plain text
+$passwordPlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+# Generate a new encryption key and IV (Initialization Vector)
+$aes = [System.Security.Cryptography.Aes]::Create()
+$encryptionKey = $aes.Key
+$encryptionIV = $aes.IV
+
+# Store the encryption key and IV securely (e.g., in a file or a secure vault)
+$encryptionKeyPath = "\\$env:DEPLOYMENT_SERVER\$env:DEPLOYMENT_PATH\Static\Keys\LocalAdminKey.bin"
+$encryptionIVPath = "\\$env:DEPLOYMENT_SERVER\$env:DEPLOYMENT_PATH\Static\Keys\LocalAdminIV.bin"
+
+# Save key files
+[System.IO.File]::WriteAllBytes($encryptionKeyPath, $encryptionKey)
+[System.IO.File]::WriteAllBytes($encryptionIVPath, $encryptionIV)
+
+# Encrypt the password
+$encryptor = $aes.CreateEncryptor($encryptionKey, $encryptionIV)
+$encryptedPasswordBytes = $encryptor.TransformFinalBlock([System.Text.Encoding]::UTF8.GetBytes($passwordPlainText), 0, $passwordPlainText.Length)
+$encryptedPassword = [Convert]::ToBase64String($encryptedPasswordBytes)
+
+# Output the encrypted password
+Write-Host "Encrypted Password: $encryptedPassword"
+
+# Dispose of AES resources
+$aes.Dispose()
---
+++ PowerShellFiles/LocalAdminPasswordSet.ps1
@@ -0,0 +1,26 @@
+# Define the path to the stored encryption Key and IV
+$encryptionKeyPath = "C:\Static\Keys\LocalAdminKey.bin"
+$encryptionIVPath = "C:\Static\Keys\LocalAdminIV.bin"
+
+# Load the encryption key and IV
+$encryptionKey = [System.IO.File]::ReadAllBytes($encryptionKeyPath)
+$encryptionIV = [System.IO.File]::ReadAllBytes($encryptionIVPath)
+
+# Get the encrypted password from the environment variable
+$encryptedPassword = $env:LOCAL_ADMIN_PASSWORD
+
+# Decrypt the password
+$aes = [System.Security.Cryptography.Aes]::Create()
+$decryptor = $aes.CreateDecryptor($encryptionKey, $encryptionIV)
+$encryptedPasswordBytes = [Convert]::FromBase64String($encryptedPassword)
+$decryptedPasswordBytes = $decryptor.TransformFinalBlock($encryptedPasswordBytes, 0, $encryptedPasswordBytes.Length)
+$decryptedPassword = [System.Text.Encoding]::UTF8.GetString($decryptedPasswordBytes)
+
+# Set the local administrator password
+$adminAccount = Get-LocalUser -Name "Administrator"
+$adminAccount | Set-LocalUser -Password (ConvertTo-SecureString -String $decryptedPassword -AsPlainText -Force)
+
+Write-Host "Local administrator password has been set successfully."
+
+# Dispose of AES resources
+$aes.Dispose()
