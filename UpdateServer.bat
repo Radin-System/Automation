9baede90b9ba5cb725d2baeef392232a63180122
Mohammad Heydari <Hmohammad2520@gmail.com>
2024-08-22 12:13:33 +0330

    Improvements

--- UpdateServer.bat
+++ UpdateServer.bat
@@ -1,8 +1,14 @@
 @ECHO OFF
 
+echo Deleting Server Files
 set SCRIPTS_PATH="\\%DEPLOYMENT_SERVER%\%DEPLOYMENT_PATH%\Static\Scripts\"
+del %SCRIPTS_PATH%\* /q
 
+echo Copying Local Files to Server
 copy "Scripts\*" %SCRIPTS_PATH% /Y
+
+echo Deleting Local Files
 del "c:\static\Scripts\*" /q
 
+echo Sync with Group Policy
 GPUpdate /force
